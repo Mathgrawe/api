9b76a2f4728c7061d9a6b4d339e280885ce2ad03
Matheus Grawe <matheus.grawe12@gmail.com>
2025-04-07 19:04:31 -0300

    adiocionado admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,4 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
     })
+    ->withProviders([
+        App\Providers\AuthServiceProvider::class,
+    ])
+    
     ->create();
